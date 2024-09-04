1ee6617cb1362bffcad8e2cc08433bb3e9acd425
M Asad Rizvi <asadraoof023@gmail.com>
2024-09-04 16:25:36 +0500

    [progress]: Optimzed the File uploading with multer and added some error handlers.

--- server/src/Routes/uploadRoutes.js
+++ server/src/Routes/uploadRoutes.js
@@ -1,42 +1,9 @@
 import { Router } from "express";
-import multer from "multer";
-import fs from "fs/promises";
-import { pdf } from "pdf-to-img";
-import path from "path";
-import { log } from "console";
-
-const storage = multer.diskStorage({
-   destination: function (req, file, cb) {
-      cb(null, "./public/uploads");
-   },
-   filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-      cb(null, file.fieldname + "-" + uniqueSuffix + "." + file.mimetype.split("/")[1]);
-   },
-});
-
-const upload = multer({ storage: storage });
+import { uploadController } from "../controller/uploadController.js";
+import { upload } from "../middleware/uploadMiddleware.js";
 
 const router = Router();
 
-router.post("/file", upload.single("resume"), async (req, res) => {
-   if (!req.file) return res.status(400).json({ error: "No file uploaded" });
-
-   if (req.file.mimetype === "application/pdf") {
-      console.log(req.file.mimetype);
-
-      const filePath = req.file.path;
-      const document = await pdf(filePath, { scale: 3 });
-
-      let counter = 1;
-      for await (const image of document) {
-         const outputPath = path.join("./public/uploads", `page${counter}.png`);
-         await fs.writeFile(outputPath, image);
-         counter++;
-      }
-   }
-
-   return res.status(200).json(req.file);
-});
+router.post("/file", upload.single("resume"), uploadController);
 
 export default router;
--- server/src/controller/uploadController.js
+++ server/src/controller/uploadController.js
@@ -1,8 +1,36 @@
-const upload = async (req, res) => {
-   try {
-      const file = req.file;
-      return res.status(200).json(file);
-   } catch (error) {
-      return res.status(500).json(error);
+import fs from "fs/promises";
+import { pdf } from "pdf-to-img";
+import path from "path";
+
+const uploadController = async (req, res) => {
+   if (!req.file) return res.status(400).json({ error: "No file uploaded" });
+
+   if (req.file.mimetype === "application/pdf") {
+      const filePath = req.file.path;
+      const document = await pdf(filePath, { scale: 3 });
+
+      let counter = 1;
+      const pdfToImages = [];
+      for await (const image of document) {
+         const outputPath = path.join(
+            "./public/uploads",
+            `page${counter}-${req.file.originalname}.png`
+         );
+         await fs.writeFile(outputPath, image);
+         pdfToImages.push({
+            desctination: "./public/uploads" + `page${counter}-${req.file.originalname}.png`,
+            mimetype: "image/png",
+            encoding: "base64",
+            path: outputPath,
+            name: `page${counter}-${req.file.originalname}.png`,
+         });
+         counter++;
+      }
+
+      return res.status(200).json(pdfToImages);
    }
+
+   return res.status(200).json(req.file);
 };
+
+export { uploadController };
