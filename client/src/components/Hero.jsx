a2e1f50044f4e14f9d0e5fab2a830f285aa23ce5
M Asad Rizvi <asadraoof023@gmail.com>
2024-09-04 09:31:43 +0500

    [feature]: Build Hero section.

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,8 +3,8 @@ import { ArrowRight } from "lucide-react";
 
 const HeroSection = () => {
    return (
-      <div className="bg-gradient-to-br from-black to-purple/5 h-screen flex items-center justify-center font-poppins text-lightGray w-full">
-         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20 text-center">
+      <div className="bg-gradient-to-br from-black to-purple/5 h-screen flex items-center justify-center font-poppins text-lightGray w-full relative">
+         <div className="max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-20 text-center bg-[url('./intersect.png')]">
             <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold mb-6">
                Create Your <span className="text-purple">Optimal CV</span>
             </h1>
@@ -12,9 +12,9 @@ const HeroSection = () => {
                Craft a professional resume that stands out and lands you your dream job.
             </p>
             <div className="flex flex-col sm:flex-row justify-center items-center gap-4">
-               <button className="bg-purple hover:bg-purple/90 text-white font-semibold py-3 px-6 rounded-full transition duration-300 ease-in-out flex items-center">
+               <button className="bg-purple hover:bg-purple/90 text-white font-semibold py-3 px-6 rounded-full transition duration-300 ease-in-out flex items-center group">
                   Get Started
-                  <ArrowRight className="ml-2 h-5 w-5" />
+                  <ArrowRight className="ml-2 h-5 w-5 -rotate-45 group-hover:translate-x-1 transition duration-300 ease-in-out group-hover:rotate-0" />
                </button>
                <button className="border border-darkGray hover:border-purple text-lightGray font-semibold py-3 px-6 rounded-full transition duration-300 ease-in-out">
                   Learn More
@@ -24,7 +24,9 @@ const HeroSection = () => {
                <p className="text-darkGray mb-4">Trusted by professionals from:</p>
                <div className="flex justify-center items-center space-x-8">
                   {/* Replace with actual company logos */}
-                  <div className="w-20 h-20 bg-darkCharcoal rounded-full"></div>
+                  <div className="w-20 h-20 bg-darkCharcoal rounded-full">
+                     <img src="https://picsum.photos/id/237/200" alt="" />
+                  </div>
                   <div className="w-20 h-20 bg-darkCharcoal rounded-full"></div>
                   <div className="w-20 h-20 bg-darkCharcoal rounded-full"></div>
                </div>
