358b795c93991cb3a91f9688adf55465f514ac54
M Asad Rizvi <asadraoof023@gmail.com>
2024-09-07 12:15:21 +0500

    [progress]: Making the best schema for auditing resume.

--- server/src/models/resumeAudit.js
+++ server/src/models/resumeAudit.js
@@ -20,8 +20,102 @@ const resumeAuditSchema = new mongoose.Schema({
       quantifiableAchievements: [
          {
             name: String,
+            summary: String,
          },
       ],
+      sentenceLength: {
+         averageLength: Number,
+         longSentences: Number,
+         shortSentences: Number,
+      },
+      industryKeywords: [
+         {
+            keyword: String,
+            isPresent: Boolean,
+         },
+      ],
+      languageTone: {
+         isFormal: Boolean,
+         tone: String,
+      },
+   },
+   skills: {
+      hardSkills: [
+         {
+            skill: String,
+         },
+      ],
+      softSkills: [
+         {
+            skill: String,
+         },
+      ],
+      skillMatchScore: {
+         type: Number,
+      },
+   },
+   resumeSections: {
+      contactInformation: {
+         type: Boolean,
+      },
+      essentialSections: [
+         {
+            section: String,
+            isPresent: Boolean,
+         },
+      ],
+      sectionLength: [
+         {
+            section: String,
+            wordCount: Number,
+         },
+      ],
+      sectionOrder: [
+         {
+            section: String,
+            isCorrectOrder: Boolean,
+         },
+      ],
+   },
+   style: {
+      resumeDesign: {
+         isGoodDesign: Boolean,
+         reason: String,
+      },
+      emailAddress: {
+         isGoodEmailDesign: Boolean,
+      },
+      activeVoiceUsage: {
+         type: Boolean,
+      },
+      buzzwordsAndCliches: {
+         hasBuzzword: Boolean,
+         buzzword: String,
+      },
+      fontAndFormatting: {
+         fontType: String,
+         fontSize: Number,
+         isConsistentFormatting: Boolean,
+      },
+      resumeLength: {
+         pages: Number,
+         isTooLong: Boolean,
+      },
+      actionVerbAnalysis: {
+         hasActionVerbs: Boolean,
+         verbFrequency: Number,
+      },
+      bulletPointUsage: {
+         hasBulletPoints: Boolean,
+         count: Number,
+      },
+   },
+   atsCompliance: {
+      isATSCompliant: Boolean,
+      reason: String,
+   },
+   readabilityScore: {
+      type: Number,
    },
 });
 
